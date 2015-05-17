6c4cb6147575c55494fd2499c21d0076cca4a7fd
Jaakko Kukkohovi <jkukkohovi@gmail.com>
2015-05-17 20:23:29 +0300

    Added unittests, more work, added supplied questions.py

---
+++ test.py
@@ -0,0 +1,247 @@
+import unittest
+import socket
+import logging
+import struct
+import errno
+from Queue import Queue
+from thingy import UDPClient
+from thingy import UDPReceiver
+
+class UDPClientTestCase(unittest.TestCase):
+    def setUp(self):
+        self.udpclient = UDPClient()
+
+    def tearDown(self):
+        self.udpclient.close()
+        self.udpclient = None
+    
+    def test_port_setting(self):
+        self.udpclient.set_listen_port()
+        self.assertEqual(10000, self.udpclient.listen_port)
+        self.udpclient.set_listen_port(1234)
+        self.assertEqual(1234, self.udpclient.listen_port)
+        
+    def test_socket_init_unbound_port(self):
+        self.udpclient.set_listen_port(10000)
+        self.assertTrue(self.udpclient.init_socket())
+    
+    def test_socket_init_bound_port(self):
+        # Bind socket to port and use same port for client
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(('localhost', 10000))
+        
+        # Try to connect the udp client to the already bound port
+        self.udpclient.set_listen_port(10000)
+        self.assertFalse(self.udpclient.init_socket())
+    
+    def test_socket_double_init(self):
+        self.udpclient.set_listen_port(10000)
+        self.assertTrue(self.udpclient.init_socket())
+        self.assertFalse(self.udpclient.init_socket())
+        
+    def test_socket_send_raw(self):
+        msg = 'TEST'
+        localhost = '127.0.0.1'
+        listenport = 10000
+        
+        # Create socket for receiving from client
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((localhost, 10001))
+        
+        
+        # Init client socket and send message
+        self.udpclient.set_listen_port(listenport)
+        self.udpclient.init_socket()
+        self.udpclient.send_raw(localhost, 10001, msg)
+        
+        (data, addr) = sock.recvfrom(64)
+        self.assertEqual(addr, (localhost, listenport))
+        self.assertEqual(data, msg)
+    
+    def test_socket_send_protocol(self):
+        msg = 'TEST'
+        localhost = '127.0.0.1'
+        listenport = 10000
+        
+        # Create socket for receiving from client
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((localhost, 10001))
+        
+        
+        # Init client socket and send message
+        self.udpclient.set_listen_port(listenport)
+        self.udpclient.init_socket()
+        self.assertTrue(self.udpclient.send_raw(localhost, 10001, msg))
+        
+        (data, addr) = sock.recvfrom(64)
+        self.assertEqual(addr, (localhost, listenport))
+        self.assertEqual(data, msg)
+        
+    def test_protocol_packing_short(self):
+        eom = False
+        ack = True
+        data = 'A'
+        packed = struct.pack('!??HH64s', eom, ack, len(data), 0, data.ljust(64, chr(0)))
+        packets = self.udpclient.packetise(False, True, 'A')
+        self.assertListEqual(packets, [packed])
+    
+    def test_protocol_packing_many(self):
+        eom = False
+        ack = True
+        data = 'A'*70
+        packed = []
+        packed.append(struct.pack('!??HH64s', eom, ack, 64, 6, data[:64]))
+        packed.append(struct.pack('!??HH64s', eom, ack, 6, 0, data[64:].ljust(64, chr(0))))
+        packets = self.udpclient.packetise(eom, ack, data)
+        self.assertListEqual(packed, packets)
+    
+    def test_protocol_unpack_short(self):
+        eom = False
+        ack = True
+        data = 'Abc'
+        packed = struct.pack('!??HH64s', eom, ack, len(data), 0, data.ljust(64, chr(0)))
+        unpacked = self.udpclient.unpack(packed)
+        self.assertTupleEqual(unpacked, (eom, ack, len(data), 0, data))
+        
+    def test_protocol_pack_unpack(self):
+        eom = False
+        ack = True
+        data = 'AbcD'*30
+        datalen = len(data)
+        udata = ''
+        packets = self.udpclient.packetise(eom, ack, data)
+        self.assertEqual(len(packets), 2)
+        ue, ua, ul, ur, uc  = self.udpclient.unpack(packets[0])
+        self.assertEqual(ue, eom)
+        self.assertEqual(ua, ack)
+        self.assertEqual(ul, 64)
+        self.assertEqual(ur, datalen-64)
+        self.assertEqual(uc, data[:64])
+        ue, ua, ul, ur, uc  = self.udpclient.unpack(packets[1])
+        self.assertEqual(ue, eom)
+        self.assertEqual(ua, ack)
+        self.assertEqual(ul, datalen-64)
+        self.assertEqual(ur, 0)
+        self.assertEqual(uc, data[64:])
+    
+    def test_socket_send_before_init(self):
+        self.assertFalse(self.udpclient.send_raw('127.0.0.1', 10001, 'TEST'))
+    
+    def test_client_receive_queue_short(self):
+        localhost = '127.0.0.1'
+        listenport = 10000
+        
+        # Create socket for sending to client
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        
+        
+        # Init client socket and start listening thread
+        self.udpclient.set_listen_port(listenport)
+        self.udpclient.init_socket()
+        
+        # Generate message in correct format for sending to client
+        eom = False
+        ack = True
+        data = 'TEST MESSAGE'
+        packed = struct.pack('!??HH64s', eom, ack, len(data), 0, data.ljust(64, chr(0)))
+        sock.sendto(packed, (localhost, listenport))
+        msg, addr = self.udpclient.get_next_message()
+        self.assertEqual(msg, data)
+        
+    def test_client_receive_queue_multi(self):
+        localhost = '127.0.0.1'
+        listenport = 10000
+        
+        # Create socket for sending to client
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        
+        
+        # Init client socket and start listening thread
+        self.udpclient.set_listen_port(listenport)
+        self.udpclient.init_socket()
+        
+        # Generate message in correct format for sending to client
+        data = 'TEST MESSAGE'*7
+        packets = self.udpclient.packetise(False, True, data)
+        for p in packets:
+            sock.sendto(p, (localhost, listenport))
+        msg, addr = self.udpclient.get_next_message()
+        self.assertEqual(msg, data)
+
+    def test_client_integrity_check_header_length(self):
+        localhost = '127.0.0.1'
+        clientport = 10000
+        testerport = 10100
+        
+        # Create socket for sending and receiving
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((localhost, testerport))
+        
+        # Init client socket and start listening thread
+        self.udpclient.set_listen_port(clientport)
+        self.udpclient.init_socket()
+        
+        # Generate message in correct format for sending to client
+        data = 'TEST MESSAGE'
+        # Pack a packet with invalid content length field, 9 instead of 12
+        packed = struct.pack('!??HH64s', False, True, len(data)-3, 0, data.ljust(64, chr(0)))
+        # Send packet
+        sock.sendto(packed, (localhost, clientport))
+        
+        # Try reading message from client
+        self.udpclient.get_next_message()
+        
+        sock.settimeout(2)
+        try:
+            bytes, addr = sock.recvfrom(128)
+            eom, ack, l, rem, msg = self.udpclient.unpack(bytes)
+            self.assertEqual(msg, 'Send again.')
+            self.assertFalse(ack)
+        except socket.error as serr:
+            self.fail('Nack receive timedout')
+        sock.close()
+
+class UDPReceiverTestCase(unittest.TestCase):
+    def setUp(self):
+        # Create test socket
+        self.addr = ('127.0.0.1', 10000)
+        self.testsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.testsocket.bind(self.addr)
+        
+        # Create test queue
+        self.queue = Queue(0)
+        self.udpr = UDPReceiver(self.testsocket, self.queue)
+
+    def tearDown(self):
+        # Stop receiver
+        self.udpr.stop()
+        self.udpr.join()
+        self.udpr = None
+        
+        # Close socket
+        self.testsocket.close()
+        self.testsocket = None
+        
+        self.queue = None
+       
+    def test_udp_receiver_thread(self):
+        # Start receiving thread
+        self.udpr.start()
+        self.assertTrue(self.udpr.is_alive())
+    
+    def test_udp_receiver_queue(self):
+        testmsg = "TEST MESSAGE"
+        
+        # Start receiving thread
+        self.udpr.start()
+        
+        # Send message to socket
+        self.testsocket.sendto(testmsg, self.addr)
+        
+        self.assertTupleEqual(self.queue.get(), (testmsg, self.addr))
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    suites = []
+    suites.append(unittest.TestLoader().loadTestsFromTestCase(UDPReceiverTestCase))
+    suites.append(unittest.TestLoader().loadTestsFromTestCase(UDPClientTestCase))
+    for s in suites:
+        unittest.TextTestRunner(verbosity=2).run(s)
--- thingy.py
+++ thingy.py
@@ -12,25 +12,182 @@ import argparse
 import socket
 import logging
 import re
+import struct
+import errno
+from Queue import Queue, Empty
+from threading import Thread
 
-class UDPClient:
-    server = {
-              'address': None,
-              'udp_port': None
-              }
+class UDPReceiver(Thread):
+    sock = None
+    stop_flag = False
+    que = None
+    
+    def __init__(self, sock, que):
+        Thread.__init__(self)
+        self.sock = sock
+        self.sock.settimeout(0)
+        self.stop_flag = False
+        self.que = que
+        
+    def run(self):
+        # No socket so stop right away
+        if not self.sock:
+            return
+        
+        # Execute until stop flag gets set
+        while not self.stop_flag:
+            try:
+                recv = self.sock.recvfrom(1024)
+                self.que.put(recv)
+                if not recv:
+                    break
+            except socket.error as err:
+                if err.errno == errno.EAGAIN:
+                    # Timeout reached, continue
+                    continue
+                break
+
+    def stop(self):
+        self.stop_flag = True
+
+class UDPClient():
+    PROTOCOL_FORMAT = '!??HH64s'
+    
     listen_port = None
+    sock = None
+    stop_flag = False
+    rq = None
+    msg_list = []
     
-    def __init__(self, listen_port = 10000):
-        self.listen_port = listen_port
+    def __init__(self):
+        self.logger = logging.getLogger('UDPClient')
+        self.logger.info('Initialized')
+        self.rq = Queue(0)
+        
+    """ Setup the UDP socket and start accepting incoming connections. Returns True on successfull 
+        socket creation and binding. False otherwise
+    """
+    def init_socket(self):
+        if self.listen_port and not self.sock:
+            try:
+                # Initialize socket
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                # Bind to local port
+                self.sock.bind(('localhost', self.listen_port))
+                # Create receiving thread and start it
+                self.receiver = UDPReceiver(self.sock, self.rq)
+                self.receiver.start()
+                return True
+            except socket.error as exc:
+                self.logger.error("Socket error: %s" % exc)
+        else:
+            self.logger.error("Connection parameters not set!")
+        return False
     
-    def 
+    def close(self):
+        try:
+            # Stop receiving thread and wait until it stops
+            self.receiver.stop()
+            self.receiver.join()
+            self.sock.close()
+            self.sock = None
+        except:
+            self.logger.error('Exception on socket closing')
+
+    def set_listen_port(self, port = 10000):
+        self.listen_port = port
+
+    def send_raw(self, address, port, data):
+        if self.sock:
+            self.sock.sendto(data, (address, port))
+            return True
+        return False
     
-    def start(self):
+    """ Returns a tuple containing packet data, source address and port: (data, (address, port))
+        Or None in case there is a problem with socket or queue
+    """
+    def get_next_message(self):
+        if not self.rq:
+            return None
+        
+        # Last received packet remaining field
+        last_remaining = None        
+        msg = ''
+        chunks = 0
+        # Read packets until queue is empty
+        while True:
+            try:
+                data, addr = self.rq.get()
+                eom, ack, chunk_len, last_remaining, msg_chunk = self.unpack(data)
+                msg_chunk = msg_chunk.rstrip(chr(0))
+                # Compare header length to stripped chunk length, if chunk length is 
+                # greater than length in header field, then we have invalid packet and
+                # need to nack to request new transmission
+                clen = len(msg_chunk)
+                if chunk_len < clen:
+                    self.logger.info('Invalid chunk length expected %d got %d' % (chunk_len, clen))
+                    p = self.packetise(False, False, 'Send again.')
+                    self.send_raw(addr[0], addr[1], p[0])
+                    return None
+                 
+                msg = msg + msg_chunk
+                chunks += 1
+                if eom:
+                    self.eom_received = True
+                if last_remaining == 0:
+                    break
+            except Empty:
+                if last_remaining is None:
+                    # No packets in queue? return None
+                    return None
+                pass
+        self.logger.info('Received %d chunks' % chunks)
+        return msg, addr
         
     
-    def set_connection_params(self, address, port):
-        self.server['address'] = address
-        self.server['udp_port'] = port
+    """ Packetises given eom, ack and content into a list of packets with content length of 64
+        each, left justified with null character padding if content is shorter than 64 bytes.
+        Packet format is [EOM:bool][ACK:bool][LEN:short][REMAINING:short][DATA:char*64]
+        Returns the list of packed packets or None if no valid packets can be created
+    """
+    def packetise(self, eom, ack, content):
+        if not (isinstance(eom, bool) and isinstance(ack, bool) and isinstance(content, str)) and len(content) > 0:
+            return None
+        
+        remaining = content
+        
+        packets = []
+        d = remaining[:64]
+        while d:
+            # Slice new remaining bytes from end
+            remaining = remaining[64:]
+            
+            # Packetise d left justified with null characters if necessary to get 64 bytes.
+            packets.append(struct.pack(self.PROTOCOL_FORMAT, eom, ack, len(d), len(remaining), d.ljust(64, chr(0))))
+            
+            # Get new packet data from remaining bytes, if any left
+            d = remaining[:64]
+        
+        return packets
+    
+    """ Unpacks given data and returns a tuple (EOM, ACK, len, remaining, content), where
+        content will be set to len length if data matches protocol format. None is returned
+        if data has no packet matching protocol format.
+    """
+    def unpack(self, data):
+        if not data:
+            return None
+        
+        # Take only amount of bytes that matches protocol format length
+        data = data[:struct.calcsize(self.PROTOCOL_FORMAT)]
+        
+        packet = None
+        try:
+            eom, ack, len, rem, content = struct.unpack(self.PROTOCOL_FORMAT, data)
+        except:
+            self.logger.error('Invalid data length!')
+        
+        return (eom, ack, len, rem, content.rstrip(chr(0)))
 
 class TCPClient:
     
@@ -84,15 +241,15 @@ class TCPClient:
             """ Valid response received, so let's try parsing the UDP port and possible capabilities
                 from it
             """
-            m = re.search('\w+ (\d+)[ ]?(\w+)', response)
+            m = re.search('\w+ (\d+)[ ]?([MCIA]*)', response)
             if m is None:
                 self.logger.info('No valid data in response')
                 return False
             else:
-                (port, capab) = m.groups()
-                self.server['udp_port'] = port
+                port, capab = m.groups()
+                self.server['udp_port'] = int(port)
                 self.server['capabilities'] = capab
-                self.logger.info('Server UDP port is %d and capabilities [%s]' % (port, capab))
+                self.logger.info('Server UDP port is %d and capabilities [%s]' % (self.server['udp_port'], self.server['capabilities']))
         else:
             self.logger.info('No data received')
         
@@ -133,8 +290,31 @@ class Thingy:
     
     def run(self):
         self.logger.info('Running...')
-        self.tcp_client.connection_request(10000)
+        
+        if not self.tcp_client.connection_request(10000):
+            self.logger.info('Unable to connect to server')
+            return 0
+        
+        # Get connection parameters from TCP client
+        server_params = self.tcp_client.get_server_params()
+        
+        # Initialize UDP server parameters and start listening
+        self.logger.info('Starting UDP server...')
+        self.udp_client.set_connection_params(server_params['address'], server_params['udp_port'])
+        self.udp_client.init_socket()
+        self.udp_client.start()
+        
         return 0;
     
 if __name__ == "__main__":
-    sys.exit(Thingy().run())
+    logging.basicConfig(level=logging.DEBUG)
+    eka = UDPClient(10000)
+    eka.init_socket()
+    toka = UDPClient(10000)
+    toka.init_socket()
+    eka.start()
+    toka.start()
+    
+    eka.send("localhost", 10010, "Moro toka!")
+    toka.send("localhost", 10000, "No moro eka!")
+    #sys.exit(Thingy().run())
